1cd615acce5fb91f6daca036d01c678f6c00820b
moranlihe <875690724@qq.com>
2019-11-08 11:07:32 +0800

    添加一点改动

--- childMavenProject/src/main/java/com/how2java/App.java
+++ childMavenProject/src/main/java/com/how2java/App.java
@@ -13,6 +13,7 @@ public class App
     public static void main( String[] args )
     {
         String dateStr = "2012-12-12 12:12:12";
+
         Date date = DateUtil.parse(dateStr);
 
         System.out.println(date);
